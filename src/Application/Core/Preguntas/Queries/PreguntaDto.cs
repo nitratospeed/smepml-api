e4e50bf84a55d44ff9ec80fabfcabc99f4646f34
Eduardo Rojas <nitratospeed@gmail.com>
2021-07-01 13:16:54 -0500

    feat: added opcionescogida

--- src/Application/Core/Preguntas/Queries/PreguntaDto.cs
+++ src/Application/Core/Preguntas/Queries/PreguntaDto.cs
@@ -12,6 +12,7 @@ namespace Application.Core.Preguntas.Queries
         public int Id { get; set; }
         public int SintomaId { get; set; }
         public string Descripcion { get; set; }
+        public string OpcionEscogida { get; set; } = "";
         public List<OpcionDto> Opciones { get; set; }
 
         public void Mapping(Profile profile)
